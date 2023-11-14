b19b7fff78078780ddecddbb46fb7c7b036dba83
Pavel Kazlou <obilonix@proton.me>
2023-11-14 16:52:32 +0300

    fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store.ts";
 import { ChakraProvider } from "@chakra-ui/react";
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <HashRouter>
-        <Provider store={store}>
-            <ChakraProvider>
+    <Provider store={store}>
+        <ChakraProvider>
+            <BrowserRouter>
                 <App />
-            </ChakraProvider>
-        </Provider>
-    </HashRouter>
+            </BrowserRouter>
+        </ChakraProvider>
+    </Provider>
 );
